867d487a68acfe3aba762e5a473c30e468b16cb2
Stefan Krüger <stefan-krueger@hotmail.de>
2017-10-01 15:41:06 +0200

    added person object to about

--- imports/ui/maincontainer/Main.jsx
+++ imports/ui/maincontainer/Main.jsx
@@ -5,22 +5,81 @@ import { createContainer } from 'meteor/react-meteor-data';
 class MainContainer extends Component {
     constructor(props) {
         super(props);
+
+        let person = {
+            name: 'Stefan',
+            profession: 'Software Engineer',
+            interests: [
+                'coding',
+                'eSports',
+                'geocaching',
+                'movies & series',
+                'VR & AR'
+            ],
+            degree: true
+        };
+
+        person.name = 'asd';
     }
 
     render() {
         return (
             <div className="main-container">
                 <div className="content-area">
-                    <div className="info-text">
+                    {
+                        (
+                        <div className="info-text">
 
-                        /* <br/>
-                        * Hi, this is me!<br/>
-                        *<br/>
-                        *<br/>
-                        *<br/>
-                        */
+                            /* <br/>
+                            &nbsp; *<br/>
+                            &nbsp; * Hi, this is me!<br/>
+                            &nbsp; *<br/>
+                            &nbsp; */
+                            <br/>
 
-                    </div>
+                            <span className='pink'>{'const '}</span>
+                            <span className='white'>{'person '}</span>
+                            <span className='pink'>{' \= '}</span>
+                            <span className='white'>&#123;</span><br/>
+
+                            <span className='indent white'>{'name'}</span>
+                            <span className='pink'>{'\: '}</span>
+                            <span className='yellow'>{' \'Stefan\' '}</span>
+                            <span className='orange'>{'\,'}</span><br/>
+
+                            <span className='indent white'>{'profession'}</span>
+                            <span className='pink'>{'\: '}</span>
+                            <span className='yellow'>{' \'Software Engineer\' '}</span>
+                            <span className='orange'>{'\,'}</span><br/>
+
+                            <span className='indent white'>{'interests'}</span>
+                            <span className='pink'>{'\: '}</span>
+                            <span className='white'>{' \[ '}</span><br/>
+
+                            <span className='array-indent yellow'>{' \'coding\' '}</span><span className='orange'>{'\,'}</span><br/>
+                            <span className='array-indent yellow'>{' \'eSports\' '}</span><span className='orange'>{'\,'}</span><br/>
+                            <span className='array-indent yellow'>{' \'geocaching\' '}</span><span className='orange'>{'\,'}</span><br/>
+                            <span className='array-indent yellow'>{' \'movies & series\' '}</span><span className='orange'>{'\,'}</span><br/>
+                            <span className='array-indent yellow'>{' \'VR & AR\' '}</span><span className='orange'>{'\,'}</span><br/>
+
+                            <span className='indent white'>{' \] '}</span>
+                            <span className='orange'>{'\,'}</span><br/>
+
+                            <span className='indent white'>{'degrees'}</span>
+                            <span className='pink'>{'\: '}</span>
+                            <span className='white'>{' \[ '}</span><br/>
+
+                            <span className='array-indent yellow'>{' \'B. ICT\' '}</span><span className='orange'>{'\,'}</span><br/>
+                            <span className='array-indent yellow'>{' \'Dipl. Ing.\' '}</span><span className='orange'>{'\,'}</span><br/>
+
+                            <span className='indent white'>{' \] '}</span>
+                            <span className='orange'>{'\,'}</span><br/>
+
+                            <span className='white'>&#125;</span><br/>
+
+                        </div>
+                        )
+                    }
                     <div className="profile-wrapper">
                         <div className="profile-image"/>
                     </div>
