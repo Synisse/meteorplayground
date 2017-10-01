92ce91cd18bc4d57c5fa4b666f0c3511c22ba9fb
Stefan Krüger <stefan-krueger@hotmail.de>
2017-10-01 02:23:30 +0200

    adds proper routing

--- imports/routes.jsx
+++ imports/routes.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Router, Route } from 'react-router';
+import { Router, Route, Redirect } from 'react-router';
 import createBrowserHistory from 'history/createBrowserHistory';
-// route components
-import App from './ui/App.jsx';
 import NotFoundPage from './ui/NotFound.jsx';
 import LandingPage from './ui/landingpage.jsx';
 import MainContainer from './ui/maincontainer/Main';
+import NavBar from './ui/navbar/Navbar';
 const browserHistory = createBrowserHistory();
 
 export const renderRoutes = () => (
     <Router history={browserHistory}>
         <div>
-            <Route exact path="/" component={MainContainer}/>
+            <NavBar/>
+            <Route exact path="/aboutme" component={MainContainer}/>
             <Route exact path="/landingpage" component={LandingPage}/>
             <Route path="/404" component={NotFoundPage}/>
+            <Redirect from="/" to="/aboutme" />
         </div>
     </Router>
 );
--- imports/ui/navbar/Navbar.jsx
+++ imports/ui/navbar/Navbar.jsx
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
 
 import { createContainer } from 'meteor/react-meteor-data';
 
 class NavbarContainer extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            selectedEntry: 'aboutme'
+        };
+    }
+
+    // workaround to trigger component rerender
+    selectEntry (aEntry) {
+        this.setState({selected: aEntry});
     }
 
     render() {
         return (
             <div className="navbar-container">
-                navbar-Container
-                <div className="aboutme" to="/">About Me</div>
-                <div className="projects" to="/projects">Projects</div>
-                <div className="contact">Contact</div>
+                <div className="nav-bar-link-container">
+                    <NavLink onClick={this.selectEntry.bind(this, 'aboutme')} activeClassName="active-navbar-item" className="link-font" to="/aboutme">About m</NavLink>
+                </div>
+                <div className="nav-bar-link-container">
+                    <NavLink onClick={this.selectEntry.bind(this, 'projects')} activeClassName="active-navbar-item" className="link-font" to="/projects">Projects</NavLink>
+                </div>
+                <div className="nav-bar-link-container">
+                    <NavLink onClick={this.selectEntry.bind(this, 'contact')} activeClassName="active-navbar-item" className="link-font" to="/contact">Contact</NavLink>
+                </div>
             </div>
         );
     }
